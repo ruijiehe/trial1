ca680e834e9e21a84e903fc0a3990661bc38f4ad
ahannon14 <ahannon14@my.whitworth.edu>
2014-05-03 13:53:47 -0700

    I've begun incorporating beta0_sendandreceive into TabTest2.  All java classes (except CallAndSms, which will be combined with MainActivity) have been included.  Android Manifest has permissions, a receiver, and a serive.  Compose_msg.xml has been changed to look right. No changed functionality.

--- TabTest2/app/src/main/java/com/example/app/MainActivity.java
+++ TabTest2/app/src/main/java/com/example/app/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.app;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
@@ -15,6 +26,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,10 +35,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
@@ -51,6 +77,13 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Line 49
+        //setComponent();
+        // From line 50 in CallAndSms.java
+        //startService(new Intent(this, MyService.class));
+        // Line 51
+        //start_recv_service();
 
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
@@ -87,7 +120,159 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
     }
 
+    /**
+     * Start receiving the service
+     * Taken from lines 70 - 77
+     */
+    /*
+    public void start_recv_service()
+    {
+        AlarmManager aManager = (AlarmManager) getSystemService(Service.ALARM_SERVICE);
+        Intent intent = new Intent(MainActivity.this,MyService.class);
+        final PendingIntent pi = PendingIntent.getService(MainActivity.this, 0, intent, 0);
+        aManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, 15000, pi);
+        //Toast.makeText(CallAndSms.this, "start service to recv unread", Toast.LENGTH_LONG).show();
+    }
 
+    public static  String getLocal(){
+        return "+8615527518807";
+    }
+    public static String getGMT(){
+        Date date = new Date();
+        Timestamp currentTimestamp = new Timestamp(date.getTime());
+        return currentTimestamp.toString();
+    }
+    public String addZoneCode(String number){//zone number will be set
+        TelephonyManager phoneMgr=(TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+
+        String countryCode = phoneMgr.getNetworkCountryIso();
+        if (number.startsWith("+"))
+            return number;
+        else
+        if (countryCode.equals("cn"))
+            return "+86".concat(number);
+        else
+            return "+1".concat(number);
+    }
+    public void saveMsg(String src,String dest,String text,String submit_time,String forward_time){
+        //insert the msg to ContentProvider
+        ContentResolver contentResolver = null;
+        contentResolver = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(MSGS.MSG._FROM,src);
+        values.put(MSGS.MSG._TO,dest);
+        values.put(MSGS.MSG._TEXT,text);
+        values.put(MSGS.MSG._SENT,submit_time);
+        values.put(MSGS.MSG._RECEIVED,forward_time);
+        //here goes the insert method;
+        contentResolver.insert(MSGS.MSG.MSGS_CONTENT_URI, values);
+        //print a notification
+        Toast.makeText(MainActivity.this,"msg saved",Toast.LENGTH_LONG).show();
+
+    }
+*/
+    /*
+    public void sendMsg(String dest_num,String text_content){//the dest number would be sent to DB for check
+
+
+        //initialize the message Structure
+        String msg_id = "2";
+    	/*
+    	 * 1 for test on PC
+    	 * 2 for test on phone
+    	 * 0 for common user*//*
+        String src = getLocal();
+        String dest = addZoneCode(dest_num);
+        String text = text_content;
+        String submit_time = getGMT();
+        String forward_time = "2014 04 03 08:49:34";
+        //save to lacal ContentProvider
+        saveMsg(src,dest,text,submit_time,forward_time);
+    	/*following is sending using HttpClient*//*
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://1.rtest2.sinaapp.com/chat/msg/");
+        String strResult = "";
+        try {
+            // Add your data
+            List<NameValuePair> msgValuePairs = new ArrayList<NameValuePair>(2);
+            msgValuePairs.add(new BasicNameValuePair("msgid", msg_id));
+            msgValuePairs.add(new BasicNameValuePair("src", src));
+            msgValuePairs.add(new BasicNameValuePair("dest", dest));
+            msgValuePairs.add(new BasicNameValuePair("text", URLEncoder.encode(text, "UTF-8")));
+            msgValuePairs.add(new BasicNameValuePair("submit_time", submit_time));
+            msgValuePairs.add(new BasicNameValuePair("forward_time", forward_time));
+            httppost.setEntity(new UrlEncodedFormEntity(msgValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            strResult = EntityUtils.toString(response.getEntity());
+//	        Toast.makeText(MainActivity.this, "received: "+strResult, Toast.LENGTH_SHORT).show();
+        } catch (ClientProtocolException e) {
+            Toast.makeText(MainActivity.this, "failed "+e.toString(), Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(MainActivity.this, "failed "+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+	    /*parsing responding json and decide whether SMS or NET*//*
+        String PATH ="";
+        try {
+            JSONObject jsonPath = new JSONObject(strResult);
+            PATH = jsonPath.getString("PATH");
+//			Toast.makeText(MainActivity.this, "PATH: "+PATH, Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            Toast.makeText(MainActivity.this, "failed "+e.toString(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+	    /*if SMS*//*
+        try {
+            if (PATH.equals("SMS")){
+                SmsManager sms = SmsManager.getDefault();
+                List<String> texts = sms.divideMessage(text);
+                for (String subtext : texts) {
+                    //the destination is got from text1
+                    sms.sendTextMessage(dest, null, subtext, null, null);
+                }
+                // note: not checked success or failure yet
+                Toast.makeText(	MainActivity.this,"Sent Via SMS",Toast.LENGTH_SHORT ).show();
+            }
+            else if(PATH.equals("NET")){
+                Toast.makeText(	MainActivity.this,"Sent Via Network",Toast.LENGTH_SHORT ).show();
+            }
+            else
+                Toast.makeText(	MainActivity.this,"PATH: "+ PATH+" end",Toast.LENGTH_SHORT ).show();
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "failed " + e.toString(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Initialize the buttons for the compose new page.
+     * Taken from lines 201 - 221 of CallAndSms.java
+     *//*
+    private void setComponent() {
+        setContentView(R.layout.compose_msg);
+        Button bt2 = (Button) findViewById(R.id.Button02);
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NewApi")
+            @Override
+            public void onClick(View v) {
+                //get the message content from text2
+                TextView messagContentView = (TextView)findViewById(R.id.editText2);
+                String smsContent = messagContentView.getText().toString();
+                //get the destination number from text1
+                TextView destContentView = (TextView)findViewById(R.id.editText1);
+                String destNumber = destContentView.getText().toString();
+
+                //send to server and save to local ContentProvider
+                sendMsg(destNumber,smsContent);
+
+            }
+        });
+
+    }
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
@@ -202,6 +387,8 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.compose_msg, container, false);
+
+
             return rootView;
         }
 
